1c39f50f9bf3da9a6eb91a125dad8be1985d39c2
Christian Fosli <christian.fosli@capgemini.com>
2023-12-08 21:33:46 +0100

    Try to impl day 8. Tests work but actual input stuck in loop

---
+++ aoc08/src/main.rs
@@ -0,0 +1,78 @@
+use std::{collections::HashMap, error::Error, fs};
+
+use itertools::Itertools;
+use regex::Regex;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input = fs::read_to_string("input.txt")?;
+    println!("Part 1: {}", steps_until_zzz(&input)?);
+    Ok(())
+}
+
+fn steps_until_zzz(s: &str) -> Result<usize, Box<dyn Error>> {
+    let map_item_re = Regex::new(r"(?<key>\w\w\w) = \((?<left>\w\w\w), (?<right>\w\w\w)\)")?;
+
+    let (lr, nodes_str) = s.split("\n\n").next_tuple().ok_or("invalid format")?;
+    let lr = lr.chars().collect::<Vec<_>>();
+
+    let nodes = nodes_str
+        .lines()
+        .map(|line| {
+            if let Some(caps) = map_item_re.captures(line) {
+                let key = caps["key"].to_owned();
+                let (left, right) = (caps["left"].to_owned(), caps["right"].to_owned());
+                Ok((key, (left, right)))
+            } else {
+                Err("Regex didn't match")
+            }
+        })
+        .collect::<Result<HashMap<_, _>, _>>()?;
+
+    let mut current_node = nodes_str.chars().take(3).collect::<String>();
+    let mut steps = 0;
+
+    while &current_node != "ZZZ" {
+        let dir = lr[steps % lr.len()];
+        current_node = nodes
+            .get(&current_node)
+            .map(|(left, right)| {
+                // println!("{current_node}=({left},{right}).\t{dir}");
+                match dir {
+                    'L' => left.clone(),
+                    'R' => right.clone(),
+                    _ => panic!("LR contained unexpected char {}", dir),
+                }
+            })
+            .ok_or("current node is missing in nodes map")?;
+        steps += 1;
+    }
+
+    Ok(steps)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::steps_until_zzz;
+
+    const TEST_MAP_1: &'static str = "RL
+
+AAA = (BBB, CCC)
+BBB = (DDD, EEE)
+CCC = (ZZZ, GGG)
+DDD = (DDD, DDD)
+EEE = (EEE, EEE)
+GGG = (GGG, GGG)
+ZZZ = (ZZZ, ZZZ)";
+
+    const TEST_MAP_2: &'static str = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)";
+
+    #[test]
+    fn it_works_1() {
+        assert_eq!(2, steps_until_zzz(TEST_MAP_1).unwrap());
+        assert_eq!(6, steps_until_zzz(TEST_MAP_2).unwrap());
+    }
+}
