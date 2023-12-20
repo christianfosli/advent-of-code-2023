b7a9fbf90d45bca6f40c9e661029ff712d30bdc3
Christian Fosli <christian.fosli@capgemini.com>
2023-12-20 13:17:54 +0100

    Finish day 9 part 1

--- aoc09/src/main.rs
+++ aoc09/src/main.rs
@@ -1,11 +1,16 @@
-use std::{error::Error, num::ParseIntError};
+use std::{error::Error, fs, num::ParseIntError};
+
+use itertools::Itertools;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    println!("Hello, world!");
+    let input = fs::read_to_string("input.txt")?;
+    let histories = parse(&input)?;
+    let part_1 = histories.iter().map(|h| predict_next(h)).sum::<isize>();
+    println!("part 1: {part_1}");
     Ok(())
 }
 
-fn parse(s: &str) -> Result<Vec<Vec<usize>>, ParseIntError> {
+fn parse(s: &str) -> Result<Vec<Vec<isize>>, ParseIntError> {
     s.lines()
         .map(|l| {
             l.split_whitespace()
@@ -15,8 +20,32 @@ fn parse(s: &str) -> Result<Vec<Vec<usize>>, ParseIntError> {
         .collect::<Result<Vec<_>, _>>()
 }
 
-fn predict_next(history: &[usize]) -> usize {
-    0
+fn predict_next(history: &[isize]) -> isize {
+    let mut extrapolation = Vec::<Vec<isize>>::new();
+    let mut extrapolation_done = false;
+    extrapolation.push(history.to_vec());
+    while !extrapolation_done {
+        let steps = extrapolation
+            .last()
+            .unwrap()
+            .iter()
+            .tuple_windows()
+            .map(|(&x1, &x2)| x2 - x1)
+            .collect::<Vec<_>>();
+
+        if steps.iter().all(|&dx| dx == 0) {
+            extrapolation_done = true
+        }
+
+        extrapolation.push(steps);
+    }
+
+    extrapolation
+        .into_iter()
+        .rev()
+        .map(|seq| *seq.last().unwrap())
+        .reduce(|val_below, val_left| val_left + val_below)
+        .unwrap()
 }
 
 #[cfg(test)]
@@ -30,6 +59,6 @@ mod tests {
     #[test]
     fn it_works_1() {
         let histories = parse(TEST_REPORT).unwrap();
-        assert_eq!(114usize, histories.iter().map(|h| predict_next(h)).sum())
+        assert_eq!(114isize, histories.iter().map(|h| predict_next(h)).sum())
     }
 }
